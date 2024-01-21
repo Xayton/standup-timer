2e18a9e36356a24a8f94ddc46fe853b3204344e9
Alberto <30591904+Xayton@users.noreply.github.com>
2024-01-21 18:16:27 +0100

    Initial commit with base project files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Stand-up Timer
+
+A simple web application that can be used to track the remaining time in a stand-up meeting, including the time spent by each person.
+
+## Build
+
+This is a simple Vue application using Vite as the build system.
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Timer from "./components/Timer.vue";
+</script>
+
+<template>
+  <img src="/timer.svg" class="logo" alt="Timer"/>
+  <Timer :seconds="50"/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+</style>
