3128ca4dcd5924bbece39c87e1f433d708c7a567
Alberto <30591904+Xayton@users.noreply.github.com>
2024-02-01 22:42:38 +0100

    Add editor config and update the workflow to include tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
+/// <reference types="vitest" />
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    base: '/standup-timer/',
-    test: {
-        // vitest specific configuration.
+  plugins: [vue()],
+  base: '/standup-timer/',
+  test: {
+    // vitest specific configuration.
 
-        // The testing environment. Simulate a DOM, so that Vue Components can be tested too.
-        environment: 'happy-dom'
-    }
+    // The testing environment. Simulate a DOM, so that Vue Components can be tested too.
+    environment: 'happy-dom'
+  }
 })
