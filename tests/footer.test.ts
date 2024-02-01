c2e6fd243d58f52b1c5af0d1f0484bf84952770f
Alberto <30591904+Xayton@users.noreply.github.com>
2024-02-01 22:42:38 +0100

    Add editor config and update the workflow to include tests

--- tests/footer.test.ts
+++ tests/footer.test.ts
@@ -5,20 +5,20 @@ import Footer from "../src/components/Footer.vue";
 import {secToTime} from "../src/composables/secToTime";
 
 test('mount component', async () => {
-    expect(Footer).toBeTruthy()
+  expect(Footer).toBeTruthy()
 
-    const wrapper = mount(Footer, {
-        props: {totalUsers: 10, totalTime: 120},
-    })
+  const wrapper = mount(Footer, {
+    props: {totalUsers: 10, totalTime: 120},
+  })
 
-    expect(wrapper.text()).toContain('10 users')
-    expect(wrapper.text()).toContain(`(${secToTime(120)})`)
+  expect(wrapper.text()).toContain('10 users')
+  expect(wrapper.text()).toContain(`(${secToTime(120)})`)
 
 
-    await wrapper.get('img').trigger('click')
-    expect(wrapper.emitted()).toHaveProperty('randomize')
+  await wrapper.get('img').trigger('click')
+  expect(wrapper.emitted()).toHaveProperty('randomize')
 
-    await wrapper.setProps({totalUsers: 2, totalTime: 15})
-    expect(wrapper.text()).toContain('2 users')
-    expect(wrapper.text()).toContain(`(${secToTime(15)})`)
+  await wrapper.setProps({totalUsers: 2, totalTime: 15})
+  expect(wrapper.text()).toContain('2 users')
+  expect(wrapper.text()).toContain(`(${secToTime(15)})`)
 })
